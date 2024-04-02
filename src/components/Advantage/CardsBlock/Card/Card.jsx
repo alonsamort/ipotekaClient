9b907cbd54845040ca6e0e16bdfc303d81860754
AlonSamort <alonsamort@.com>
2024-04-02 00:12:11 -0700

    done 1 cards

--- src/components/Advantage/CardsBlock/Card/Card.jsx
+++ src/components/Advantage/CardsBlock/Card/Card.jsx
@@ -5,7 +5,14 @@ function Card(props) {
     return (
         <>
             <div className={cn(styles.wrap, props.className)}>
-                <img src="/cup.png" alt="кубок"/>
+                <img className={cn(styles.img, props.img)} src="/list.png" alt="список" width={"124px"} height={"124px"}/>
+                <div className={styles.content}><h2 className={cn(styles.title, props.title)}> Прозрачность и доверие</h2>
+                    <p className={cn(styles.description, props.description)}>Наша работа основана на прозрачных <br/>процессах и честной
+                        коммуникации</p>
+                    <p className={styles.more}>читать больше</p>
+                </div>
+
+
             </div>
         </>
 
