ecf9d7e2e0a26d6d601d4a85254c568b0c0fb9aa
alonsamort <alonsamort@gmail.com>
2024-04-09 00:52:55 +0300

    add component percentage for stages

--- src/components/Stages/Card/Card.jsx
+++ src/components/Stages/Card/Card.jsx
@@ -3,13 +3,17 @@ import styles from './Card.module.css';
 import cn from "classnames";
 import Heading from "../../Stages/Card/Heading";
 import Advantage from "../../Stages/Card/Advantage";
+import Percentage from "../../Stages/Card/Percentage";
+
 function Card(props) {
     return (
         <>
             <div className={cn(styles.wrap, props.className)}>
+                <Percentage {...props.circle}/>
                 < Heading {...props.heading} />
 
-                <Advantage className={styles.advantage} advantages={props.advantages} />
+
+                <Advantage className={styles.advantage} advantages={props.advantages}/>
             </div>
         </>
 
