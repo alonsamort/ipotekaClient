96b1369ae7ed74e002a9a8532688439d998f9422
alonsamort <alonsamort@gmail.com>
2024-04-10 00:16:26 +0300

    fix invalid input in form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -15,31 +15,47 @@ function Form(props) {
 
     return (
         <>
-        <div className={cn(styles.wrap, props.className)}>
+            <div className={cn(styles.wrap, props.className)}>
 
-            <Header className={styles.header} title="Плохая кредитная история, отказали несколько банков,
+                <Header className={styles.header} title="Плохая кредитная история, отказали несколько банков,
 или нет первого взноса?"
-                    subtitle="Не беспокойтесь! Наши специалисты успешно решают сложные ситуации с ипотекой в любом регионе России. Работаем до положительного решения банка. Заполните форму, и вам окажут помощь в решении всех вопросов!"/>
+                        subtitle="Не беспокойтесь! Наши специалисты успешно решают сложные ситуации с ипотекой в любом регионе России. Работаем до положительного решения банка. Заполните форму, и вам окажут помощь в решении всех вопросов!"/>
 
 
-            <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-                <input type="tel" className={`${styles.inputName} ${errors.firstName ? styles.errorInput : ''}`}
-                       {...register('firstName', { required: "Поле необходимо заполнить", minLength: {
-                               value: 3,
-                               message: "Поле слишком короткое"
-                           }   })}
+                <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+                    <input
+                        type="tel"
+                        className={`${styles.inputName} ${errors.firstName ? styles.errorInput : ''}`}
+                        {...register('firstName', {
+                            required: "Поле необходимо заполнить",
+                            minLength: {
+                                value: 3,
+                                message: "Поле слишком короткое"
+                            }
+                        })}
+                        placeholder="Имя"
+                    />
+                    {errors.firstName && (
+                        <div className={styles.error}>
+                            {errors.firstName.message}
+                        </div>
+                    )}
 
-                       placeholder="Имя"/>
-                {errors.firstName && <div className={styles.error}>Поле необходимо заполнить</div>}
-                <input type="tel" className={`${styles.inputPhone} ${errors.telephone ? styles.errorInput : ''}`}
-                       {...register('telephone', { required: true })}
-                       placeholder="+7 (999) 999-99-99"/>
-                {errors.firstName && <div className={styles.error}>Поле необходимо заполнить</div>}
-                <input className={styles.button} type="submit"/>
+                    <input
+                        type="tel"
+                        className={`${styles.inputPhone} ${errors.telephone ? styles.errorInput : ''}`}
+                        {...register('telephone', {required: true})}
+                        placeholder="+7 (999) 999-99-99"
+                    />
+                    {errors.telephone && (
+                        <div className={styles.error}>
+                            Поле необходимо заполнить
+                        </div>
+                    )}
 
-
-            </form>
-        </div>
+                    <input className={styles.button} type="submit"/>
+                </form>
+            </div>
         </>
 
     )
